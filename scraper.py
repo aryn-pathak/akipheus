c3e6e916899583ca13339443b94ffb8ef14df311
aryan pathak <thearyanpathak@gmail.com>
2026-03-15 20:29:23 +0530

    edited the json logic (earlier logic wouldn't have given the full json), added support for opening it as a dataframe

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
 # this is a scraper to get entries from wikidata.
-# akigator will only use "humans" (non-fictional humans) with >15 sitelinks (somewhat popular, not including ultra-niche people), and having a defined gender, citizenship, occupation (to filter out ghosts), and has a follower count listed
+# akigator will only use "humans" (non-fictional humans) with >25 sitelinks (somewhat popular, not including ultra-niche people), and having a defined gender, citizenship, occupation (to filter out ghosts), and has a follower count listed
 # using a scraper means we'll get a list of entities much, much smaller than wikidata's list, and doesn't query wikidata to sort through 120M+ entities for every small question, which is abusive to them.
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
+import pandas as pd
 
 endpoint_url = "https://query.wikidata.org/sparql"
-sparql = SPARQLWrapper(endpoint_url, agent="Akigator/1.0 (thearyanpathak@gmail.com) An open source educational project to recreate how Akinator (classic character guessing game) works. Uses Wikidata's database on humans instead of proprietary closed-source character information like akinator.")
+sparql = SPARQLWrapper(endpoint_url, agent="Akigator/1.0 (thearyanpathak@gmail.com) An open source educational project to recreate how Akinator (classic character guessing game) works. Uses Wikidata's database on humans instead of proprietary closed-source character information like akinator. Not spam, this is a one time thing but will take some time, so will require a longer timeout")
 
 query = """
 SELECT ?person ?personLabel ?personDescription ?sex ?occupation ?citizenship ?sitelinks (MAX(?rawCount) AS ?followers)
@@ -21,7 +22,7 @@ WHERE {
  # OPTIONAL { ?person wdt:P101 ?fieldExists . }
  # BIND(COALESCE (?fieldExists, 'NIL') as ?field)
           
-  FILTER(?sitelinks > 20)
+  FILTER(?sitelinks > 25)
   FILTER(?rawCount > 100000)
             
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
@@ -32,6 +33,12 @@ sparql.setQuery(query)
 sparql.setReturnFormat(JSON)
 
 results = sparql.query().convert()
+
+resultsList = []
 for result in results["results"]["bindings"]:
-    with open("humans.json", "w") as humans:
-        json.dump(result, humans, indent=4)
+    resultsList.append(result)
+
+with (open('humans.json', 'w') as humans):
+    json.dump(resultsList, humans, indent=4)
+
+df=pd.read_json("humans.json")
