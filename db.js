dfabb10cc28cbae2d37e8679b618a4e00a4507ea
aryan pathak <thearyanpathak@gmail.com>
2026-03-28 23:49:49 +0530

    made the sql work lol. working on a function now

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
+let db;
 async function init(){
+    console.log("Akigator initializing, please wait!");
     const SQL = await initSqlJs({
         locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.12.0/${file}`
     });
+
     const response = await fetch("humans.db")
     const buffer = await response.arrayBuffer();
-    const db = new SQL.Database(new Uint8Array(buffer));
+    db = new SQL.Database(new Uint8Array(buffer));
+    console.log("welcome to Akigator!");
 }
-init();
 
-const results = db.exec("SELECT * FROM humansFlat LIMIT 10");
-console.log(results);
+// dict is a list expected to be in format [{"sex" : "male"}, {"citizenship" : "NOT india, NOT united states, NOT canada, france, italy"}, {"occupation" : "NOT politician, NOT scientist, actor"}]
+// anything with NOT is something answered "no" to.
+
+// export function getAll(dict){
+//     const sex = dict.sex
+//     const
+//
+//     return results;
+// }
+
+init().then(()=>{console.log(getAll("nigger"))})
