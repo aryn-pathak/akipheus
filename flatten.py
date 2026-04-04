dc6ba06610c34f7aa2ee1d9162eef6a56fc2cfd6
aryan pathak <thearyanpathak@gmail.com>
2026-04-04 16:06:14 +0530

    changed sitelinks weighting logic. using defined tiers of followers, and decreasing sitelink weights by tiers.

--- flatten.py
+++ flatten.py
@@ -7,9 +7,6 @@ import sqlite3
 import numpy as np
 import pandas as pd
 
-slWeightFollowers = 1
-slWeightNoFollowers = 2
-
 con = sqlite3.connect("humans.db")
 df = pd.read_sql("SELECT * FROM humansRaw", con)
 df = df.drop_duplicates(subset=['occupationLabel', 'citizenshipLabel', 'sexLabel', 'sitelinks', 'followers'])
@@ -23,11 +20,27 @@ grouped = df.groupby('personLabel').agg({
     'followers': 'first'
 }).reset_index()
 
-grouped['P'] = np.where(
-    grouped['followers'] > 0,
-    grouped['followers'] + grouped['sitelinks']*slWeightFollowers,
-    grouped['sitelinks']*slWeightNoFollowers
-)
+conditions = [
+    (grouped['followers'] == 0),
+    (grouped['followers'] <= 1_000_000),
+    (grouped['followers'] <= 5_000_000),
+    (grouped['followers'] <= 25_000_000),
+    (grouped['followers'] <= 100_000_000),
+    (grouped['followers'] <= 300_000_000)
+    (grouped['followers'] > 300_000_000)
+]
+choices = [
+    150_000 + (grouped['sitelinks'] * 50_000),
+    grouped['followers'] + (grouped['sitelinks'] * 50_000),
+    grouped['followers'] + (grouped['sitelinks'] * 25_000),
+    grouped['followers'] + (grouped['sitelinks'] * 10_000),
+    grouped['followers'] + (grouped['sitelinks'] * 5_000),
+    grouped['followers'] + (grouped['sitelinks'] * 2_000),
+]
+
+default = grouped['followers'] + (grouped['sitelinks'] * 0)
+
+grouped['P'] = np.select(conditions, choices, default=default)
 
 grouped.to_sql("humansFlat", con, if_exists='replace', index=False)
 
