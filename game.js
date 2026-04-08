4adf9e4c639264f5ff3287b22287b7697f9bf79b
aryan pathak <thearyanpathak@gmail.com>
2026-04-08 17:39:06 +0530

    S and R in aggregate

--- game.js
+++ game.js
@@ -16,32 +16,32 @@ function bayesian(M, C, S, R){          // M: avg P of all, C: weight, S: sum of
     return ((C*M)+(S*R))/(C+S)
 }
 
-function getPopular(property, obj){         // property is the key name. obj is obj. func is getAll()
+// citizenshipLabel is [4], occupationLabel is [3]. getAll() returns columns and values separately, values is an array, not key-value pairs
+function getPopular(property, obj){         // property is the key name (citizenshipLabel or occupationLabel), obj is obj.
     let people = getAll(obj)[0].values
     let index
     let raw = []
-    let Unique = []
-    let PList = []
 
     index = (property === "citizenshipLabel") ? 4 : 3;
 
     people.forEach((item) => {
         const object = {"name":item[index], "P":item[7]}
-        raw.push(...object)
+        raw.push(object)
     })
 
     let unique = []
     raw.forEach((item) => {
-        if (item.name in unique){}else{
+        if (unique.includes(item.name)){}else{
             unique.push(item.name)
         }
     })
+
+    let aggregate = []
     for (const item in unique){
-       let filtered = raw.filter(object => object.name === item)
-        // use .reduce() to sum up P, get count of each occupation/country
+       let filtered = raw.filter(o => o.name === item).map(o => o.P);
+       let totalP = filtered.reduce((acc, no)=>acc + no);
+       aggregate.push({"name":item, "S":totalP, "R":(totalP/filtered.length)});
     }
 }
 
-// citizenshipLabel is [4], occupationLabel is [3]. getAll() returns columns and values separately, values is an array, not key-value pairs
-
-//  return people.filter((item) => item.citizenshipLabel.includes(country));
+//  return people.filter((item) => item[4].includes(country));
