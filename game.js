897970d7a9737cb53e497b6f3e5f4d9e69fb4f6a
aryan pathak <thearyanpathak@gmail.com>
2026-04-13 20:05:36 +0530

    fixed getQuestion logic, still probably not perfect

--- game.js
+++ game.js
@@ -23,63 +23,77 @@ function waitForClick() {
     });
 }
 
-function getQuestion(){
+async function getQuestion() {
     let effectProperty;
     let highEffect = -Infinity;
 
     if (obj.special.includes("employed")) properties.push("employed")
     if (obj.special.includes("politician")) properties.push("politician")
 
-    for(const property of properties){
-        let people = { ...obj, [property]: obj[property].concat(getPopular(property, obj)) }
+    for (const property of properties) {
+        let people = {...obj, [property]: obj[property].concat(getPopular(property, obj))}
         let yes = getAll(people)[0].values.count
-        people = { ...obj, [property]: obj[property].concat("NOT " + getPopular(property, obj)) }
+        people = {...obj, [property]: obj[property].concat("NOT " + getPopular(property, obj))}
         let no = getAll(people)[0].values.count
-        let effect = getAll(obj)[0].values.count - ((yes + no)/2)
+        let effect = getAll(obj)[0].values.count - ((yes + no) / 2)
 
-        if(effect > highEffect){
+        if (effect > highEffect) {
             highEffect = effect
             effectProperty = property
-        }else{}
+        }
     }
-    if(highEffect <= 6){
-        return effectProperty
-    }else if(getAll(obj)[0].values.count === 1){
+    if (getAll(obj)[0].values.count === 1) {
         return "complete"
-    }
-    else if(getAll(obj)[0].values.count === 0){return "none"}
-
-    else if(highEffect <= 5){
+    } else if (getAll(obj)[0].values.count === 0) {
+        return "none"
+    }else if (highEffect <= 5) {
         let descList = []
-        for (const person of getAll(obj)[0].values){
-            descList.push({'person':person[0], 'desc':person[1], 'occupations':person[3], 'P':person[7]}) // change index for occupationLabel, P.
+        for (const person of getAll(obj)[0].values) {
+            descList.push({'person': person[0], 'desc': person[1], 'occupations': person[3], 'P': person[7]}) // change index for occupationLabel, P.
         }
         let questionList = getDesc(descList)
 
         let personIndex = 0;
         let questionIndex = 0
 
-        function showQuestion(){question.innerHTML = questionList[personIndex][questionIndex];}
+        function showQuestion() {
+            question.innerHTML = questionList[personIndex][questionIndex];
+        }
+
         showQuestion();
 
-        yesButton.addEventListener("click", function(){
-            if(questionIndex===questionList[personIndex].questions.length-1){question.innerHTML = `you're thinking of ${questionList[personIndex].person}`}
-            else{questionList[personIndex].yes += 1}
-            questionIndex += 1
-            if (questionIndex === questionList[personIndex].questions.length-1){personIndex += 1; questionIndex = 0; showQuestion()}
-            if (personIndex === questionList.length){
-                question.innerHTML = `you're thinking of ${questionList.reduce((a, b) => {return (a.yes > b.yes) ? a:b}).person}`
+        let answer = await waitForClick()
+        if(answer==="yes"){
+            if (questionIndex === questionList[personIndex].questions.length - 1) {
+                question.innerHTML = `you're thinking of ${questionList[personIndex].person}`
+            } else {
+                questionList[personIndex].yes += 1
             }
-        })
-        noButton.addEventListener("click", function(){
             questionIndex += 1
-            if (questionIndex === questionList[personIndex].length-1){personIndex += 1; questionIndex = 0;}
+            if (questionIndex === questionList[personIndex].questions.length - 1) {
+                personIndex += 1;
+                questionIndex = 0;
+                showQuestion()
+            }
+            if (personIndex === questionList.length) {
+                question.innerHTML = `you're thinking of ${questionList.reduce((a, b) => {
+                    return (a.yes > b.yes) ? a : b
+                }).person}`
+            }
+        }else{
+            questionIndex += 1
+            if (questionIndex === questionList[personIndex].length - 1) {
+                personIndex += 1;
+                questionIndex = 0;
+            }
             showQuestion()
-            if (personIndex === questionList.length){
-                question.innerHTML = `you're thinking of ${questionList.reduce((a, b) => {return (a.yes > b.yes) ? a:b}).person}`
+            if (personIndex === questionList.length) {
+                question.innerHTML = `you're thinking of ${questionList.reduce((a, b) => {
+                    return (a.yes > b.yes) ? a : b
+                }).person}`
             }
-        })
-    }
+        }
+    }else{return effectProperty}
 }
 async function special(){
     if(obj.special.length <= 0){
@@ -100,18 +114,17 @@ async function generate() {
     start.style.display = "none";
 
     await special()
-    let property = getQuestion();
+    let property = await getQuestion();
     let value = getPopular(property, obj)
     if(property === "complete"){question.innerHTML = "you're thinking of" + getAll(obj)[0].values[0][0] + "!"}
     else if(property === "none") question.innerHTML = "sorry, I couldn't guess your character :("
     else if(property in statements){
-
-        question.innerHTML = statements[property] + value + "?"  // ✅ keep this
+        question.innerHTML = statements[property] + value + "?"
         const answer = await waitForClick();
         if (answer === "yes") { obj[property].push(value) }
         else { obj[property].push("NOT " + value) }
         await generate();
-    }else{getQuestion();}
+    }else{await getQuestion();}
 }
 
 init().then(() => {
