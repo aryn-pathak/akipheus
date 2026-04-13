3fc48092b3ed92424d65ac8c242375bfd518587f
aryan pathak <thearyanpathak@gmail.com>
2026-04-13 20:53:48 +0530

    fixed small problems from review

--- db.js
+++ db.js
@@ -1,5 +1,30 @@
 let db
-export let obj = {"citizenshipLabel":[], "sexLabel":[], "occupationLabel":[], "field":[], "special":[], "political party":[], "employer":[], "alive":[]};
+export let obj = {"citizenshipLabel":[], "sexLabel":[], "occupationLabel":[], "special":[], "political party":[], "employer":[], "alive":[], "field":[]};
+const indices = {
+    'citizenshipLabel':1,
+    'sexLabel':2,
+    'occupationLabel':3,
+    'special':4,
+    'political party':5,
+    'employer':6,
+    'alive':7,
+    'P':11,
+    'field':12
+}
+
+function match(name) {
+    let item = getAll(obj)[0].values.find(o => o[0] === name);
+    let totalNo = 0;
+    let no = 0;
+
+    for (const property of Object.keys(obj)) {
+        let array = item[indices[property]];
+        totalNo += array.filter(o => obj[property].includes(o)).length;
+        no += obj[property].length;
+    }
+    return totalNo / no;
+}
+
 import nlp from 'compromise'
 nlp.addWords({
     'prime minister':    'Title',
@@ -38,7 +63,7 @@ export async function init(){
 
 export function getAll(o) {
     let query = "SELECT * FROM humansFlat WHERE sitelinks > 1"
-    for (let key of o) {
+    for (let key of Object.keys(o)) {
         for (let filter of o[key]) {
             if (filter.startsWith("NOT "))
                 query += ` AND ${key} NOT LIKE '%"${filter.slice(4)}"%'`;
@@ -46,13 +71,20 @@ export function getAll(o) {
         }
     }
     let result = db.exec(query);
+    if (!result || result.length === 0) return [];
     console.log(result);
 
     let rows = result[0].values
 
     for (const item of rows) {
+        item[2] = item[2] ? JSON.parse(item[2]) : [];
         item[3] = item[3] ? JSON.parse(item[3]) : [];
         item[4] = item[4] ? JSON.parse(item[4]) : [];
+        item[5] = item[5] ? JSON.parse(item[5]) : [];
+        item[6] = item[6] ? JSON.parse(item[6]) : [];
+        item[7] = item[7] ? JSON.parse(item[7]) : [];
+        item[8] = item[8] ? JSON.parse(item[8]) : [];
+        item[12] = item[12] ? JSON.parse(item[12]) : [];
     }
     return result;
 }
@@ -64,14 +96,8 @@ export function getPopular(property, obj){
     let people = getAll(obj)[0].values
     let index
     let raw = []
-
-    index = (property === "citizenshipLabel") ? 4 : 3; // need to change indices to match, allow sex/citizenship/occupation/field/political party/employer
-
     people.forEach((item) => {
-        for(name of item){
-            const object = {"name": name, "P":item[7]};  // change to P's index
-        }
-        raw.push(object)
+        raw.push({"name": item[0], "P": item[indices.P]})
     })
 
     let unique = []
@@ -80,13 +106,13 @@ export function getPopular(property, obj){
             unique.push(item.name)
         }
     })
-    const M = raw.reduce((acc, o)=>acc + o.P, 0);/unique.length
+    const M = raw.reduce((acc, o)=>acc + o.P, 0)/unique.length
 
     let aggregate = []
     for (const item of unique){
         let filtered = raw.filter(o => o.name === item).map(o => o.P);
         let totalP = filtered.reduce((acc, no)=>acc + no);
-        aggregate.push({"name":item, "S":filtered.length, "B":bayesian(M, C, totalP, (totalP/filtered.length))});
+        aggregate.push({"name":item, "S":filtered.length, "B":bayesian(M, totalP, (totalP/filtered.length))});
     }
     aggregate.sort((a, b)=>b.B-a.B)
 
@@ -99,27 +125,28 @@ export function getPopular(property, obj){
     }
     return result;
 }
+
 export function getDesc(descList){
     let list = []
-    let returnList = []
     for (const person of descList){
-        let desc = descList.desc
+        let desc = person.desc
 
         let organisation = desc.organisations().out('array')
         let nouns = desc.nouns().out('array').filter(item =>
             !obj.occupationLabel.includes(item) &&
             !obj.occupationLabel.includes(`NOT ${item}`)
         ) // removes occupations already filtered, leaves titles and uncovered occupations
-        list.push({'person':person.person, 'organisation':organisation,'nouns':nouns, 'P':person.P})
+        list.push({'person':person[person], 'organisation':organisation,'nouns':nouns, 'match':match(person.person)})
     }
-    list.sort((a, b) => b.P - a.P)
-    for(const item of list){delete item.P}
+    list.sort((a, b) => b.match - a.match)
+    for(const item of list){delete item.match}
 
     let questions = []
     for (const item of list){
-        let questionsList = {'person':item.person, 'questions':[]}
+        let questionsList = {'person':item.person, 'questions':[], 'yes': 0}
         for (const org of item.organisation){questionsList['questions'].push(`is your character associated with ${org}`)}
         for (const noun of item.nouns){questionsList['questions'].push(`is your character associated with ${noun}`)}
+        questions.push(questionsList);
     }
     return questions
 }
